7a139219871815cd2232ec80ad4b35df051e42e1
Guillaume BINET <gbin@gootz.net>
2013-01-03 14:59:37 -0800

    added py2 compat

--- tests/test_codebot.py
+++ tests/test_codebot.py
@@ -4,7 +4,7 @@ from errbot.backends.test import FullStackTest
 class TestCommands(FullStackTest):
     @classmethod
     def setUpClass(cls, extra=None):
-        super().setUpClass(__file__)
+        super(TestCommands, cls).setUpClass(__file__)
 
     def test_python(self):
         self.assertCommand('!python print 1+1', '2', timeout=15)
