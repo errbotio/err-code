0009543c8769fced434e5b849041a79a40229fb3
Guillaume Binet <gbin@gootz.net>
2015-05-31 12:31:20 -0700

    remove testing crash

--- tests/test_codebot.py
+++ tests/test_codebot.py
@@ -2,9 +2,6 @@
 from errbot.backends.test import FullStackTest
 
 class TestCommands(FullStackTest):
-    @classmethod
-    def setUpClass(cls, extra=None):
-        super(TestCommands, cls).setUpClass(__file__)
 
     def test_python(self):
         self.assertCommand('!python print 1+1', '2', timeout=15)
